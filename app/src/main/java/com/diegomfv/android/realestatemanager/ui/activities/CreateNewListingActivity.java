6b81b43fac0cd29358a6479a43853d2379738a55
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-08-18 17:33:33 +0100

    Creating CreateNewListingActivity

--- app/src/main/java/com/diegomfv/android/realestatemanager/ui/activities/CreateNewListingActivity.java
+++ app/src/main/java/com/diegomfv/android/realestatemanager/ui/activities/CreateNewListingActivity.java
@@ -3,9 +3,14 @@ package com.diegomfv.android.realestatemanager.ui.activities;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.diegomfv.android.realestatemanager.R;
+
+import butterknife.BindView;
 
 /**
  * Created by Diego Fajardo on 18/08/2018.
@@ -15,6 +20,37 @@ import com.diegomfv.android.realestatemanager.R;
 public class CreateNewListingActivity extends AppCompatActivity {
 
     private static final String TAG = CreateNewListingActivity.class.getSimpleName();
+
+    /////////////////////////////////
+
+    @BindView(R.id.text_input_ac_text_view_type_of_bulding_id)
+    TextView tvTypeOfBuilding;
+
+    @BindView(R.id.text_input_ac_text_view_price_id)
+    TextView tvPrice;
+
+    @BindView(R.id.text_input_ac_text_view_surface_area_id)
+    TextView tvSurfaceArea;
+
+    @BindView(R.id.text_input_ac_text_view_number_of_rooms_id)
+    TextView tvNumberOfRooms;
+
+    @BindView(R.id.text_input_ac_text_view_description_id)
+    TextView tvDescription;
+
+    @BindView(R.id.text_input_layout_address_id)
+    TextView tvAddress;
+
+    @BindView(R.id.recyclerView_media_id)
+    RecyclerView recyclerView;
+
+    @BindView(R.id.button_add_photo_id)
+    Button buttonAddPhoto;
+
+    @BindView(R.id.button_insert_listing_id)
+    Button buttonInsertListing;
+
+    /////////////////////////////////
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,9 +64,5 @@ public class CreateNewListingActivity extends AppCompatActivity {
 
 
     }
-
-
-
-
 
 }
