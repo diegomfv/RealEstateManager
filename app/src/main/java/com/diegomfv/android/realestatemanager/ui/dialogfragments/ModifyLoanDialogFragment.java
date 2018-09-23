2afbe52e9db6dd3661ab512a2aab07198b4f028f
diegoma77 <31185512+diegoma77@users.noreply.github.com>
2018-09-23 12:04:08 +0100

    Finish Loan Simulator Activity

---
+++ app/src/main/java/com/diegomfv/android/realestatemanager/ui/dialogfragments/ModifyLoanDialogFragment.java
@@ -0,0 +1,7 @@
+package com.diegomfv.android.realestatemanager.ui.dialogfragments;
+
+/**
+ * Created by Diego Fajardo (https://github.com/diegomfv) on 23/09/2018.
+ */
+public class ModifyLoanDialogFragment {
+}
